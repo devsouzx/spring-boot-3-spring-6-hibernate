998a19f7adcb970b7ded4e26ae784cc771e13838
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-06-21 06:43:08 -0300

    Qualifiers

--- 02-spring-boot-spring-core/src/main/java/com/devsouzx/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-spring-core/src/main/java/com/devsouzx/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.devsouzx.springcoredemo.rest;
 
 import com.devsouzx.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,7 @@ public class DemoController {
     private Coach coach;
 
     @Autowired
-    public DemoController(Coach coach) {
+    public DemoController(@Qualifier("tennisCoach") Coach coach) {
         this.coach = coach;
     }
 
