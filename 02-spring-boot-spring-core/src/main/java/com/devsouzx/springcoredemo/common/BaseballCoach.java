8174bac79fe24981491f816be7bb924cdbbd62db
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-06-21 07:02:36 -0300

    Lazy initialiazation

--- 02-spring-boot-spring-core/src/main/java/com/devsouzx/springcoredemo/common/BaseballCoach.java
+++ 02-spring-boot-spring-core/src/main/java/com/devsouzx/springcoredemo/common/BaseballCoach.java
@@ -1,10 +1,13 @@
 package com.devsouzx.springcoredemo.common;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BaseballCoach implements Coach {
+    public BaseballCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Spend 30 minutes in batting practice";
